165a8868d3a05d1fb01c15c8cf184ea65e79644f
trinhcuong297 <trinhcuong297@gmail.com>
2022-11-13 09:26:06 +0700

    googleVerifi

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <title>Online simulator circuit</title>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <meta />
+          <meta
+            name="google-site-verification"
+            content="K_pPeGKApk94IcoK4yBsUlZo2qbRkuDENbxfu6WOVp4"
+          />
+          <meta name="description" content="Simple online simulator circuit" />
+          <meta name="geo.region" content="VN-HN" />
+          <meta name="geo.placename" content="Hanoi" />
+          <meta name="geo.position" content="21.004296;105.843723" />
+          <meta name="ICBM" content="21.004296, 105.843723" />
+        </Head>
         <body>
           <Main />
           <NextScript />
